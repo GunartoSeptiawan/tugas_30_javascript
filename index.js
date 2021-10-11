7a9eaef083f87574d1495e828b6960414f9651d7
Gunarto Septiawan <gunartosept@gmail.com>
2021-10-11 16:32:30 +0700

    tugas_30_javascript
    Tugas Niomic

---
+++ index.js
@@ -0,0 +1,9 @@
+function regex() {
+  let str =
+    "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat";
+
+  console.log(str.match(/[es]/g));
+  // console.log(str.match(/s/g));
+}
+
+regex();
